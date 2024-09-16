fe68a32c3ba2bc26bba29fddcdbed6e115a50576
Paulojoserc <pjrc83@gmail.com>
2024-09-16 17:43:08 -0300

    Algaworks mudulo 5 atividade Switch

--- bin/Algaworks_CursoLogicaCapitulo05/src/Atividade/EstruturaIf.java
+++ bin/Algaworks_CursoLogicaCapitulo05/src/Atividade/EstruturaIf.java
@@ -4,15 +4,18 @@ public class EstruturaIf {
 	public static void main(String[] args) {
 		Double emprestimo = 4000.0;
 		Double movimentacaoMedia = 2000.0;
-		
-		Boolean movimentaPeloMenosMetadeDoValor = true;
+
+		//Boolean movimentaPeloMenosMetadeDoValor = (movimentacaoMedia * 2) >= emprestimo;
 		Boolean aContaTemTempoSuficenteDeAbertura = true;
 		Boolean temNomeLimpo = true;
-		
-		//Boolean liberarEmprestimo = movimentaPeloMenosMetadeDoValor && aContaTemTempoSuficenteDeAbertura && temNomeLimpo ;
-		
-		if (movimentaPeloMenosMetadeDoValor && aContaTemTempoSuficenteDeAbertura && temNomeLimpo) {
+
+		// Boolean liberarEmprestimo = movimentaPeloMenosMetadeDoValor &&
+		// aContaTemTempoSuficenteDeAbertura && temNomeLimpo ;
+
+		if (((movimentacaoMedia *2) >= emprestimo) && aContaTemTempoSuficenteDeAbertura && temNomeLimpo) {
 			System.out.println("Sim! Pode liberar emprestimo");
-	}
+		} else {
+			System.out.println("Não pode liberar o empréstimo.");
+		}
 	}
 }
