6de627a893e9f71152af9419fdcddc2e6e40efa1
Paulojoserc <pjrc83@gmail.com>
2024-09-17 17:44:18 -0300

    Algaworks mudulo 5 ex03 Switch

---
+++ bin/Algaworks_CursoLogicaCapitulo05/src/Exercicio/Ex03.java
@@ -0,0 +1,43 @@
+package Exercicio;
+
+import java.util.Scanner;
+
+public class Ex03 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Digite os dia que deseja");
+	Integer diasDaSemana = sc.nextInt();
+	
+	switch(diasDaSemana){
+	case 1:
+		System.out.println("Domingo");
+		break;
+	case 2:
+		System.out.println("Segunda");
+		break;
+	case 3:
+		System.out.println("Terça");
+		break;
+	case 4:
+		System.out.println("Quarta");
+		break;
+	case 5:
+		System.out.println("Quinta");
+		break;
+	case 6:
+		System.out.println("Sexta");
+		break;
+	case 7:
+		System.out.println("Sabado");
+		break;
+
+	default: System.err.println("Digite um dia válido!");	
+	}
+	
+	System.exit(1);
+			sc.close();
+}
+
+
+}
