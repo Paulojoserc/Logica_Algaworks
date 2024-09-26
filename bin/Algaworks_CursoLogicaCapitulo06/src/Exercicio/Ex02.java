a6ce3d3fd95e5120812f524dbe121204a2e4487f
Paulojoserc <pjrc83@gmail.com>
2024-09-26 18:20:27 -0300

    Algaworks finalização do mudulo 6

---
+++ bin/Algaworks_CursoLogicaCapitulo06/src/Exercicio/Ex02.java
@@ -0,0 +1,16 @@
+package Exercicio;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+		int i = 1;
+		while (i <= numeros.length) {
+
+			if (i % 3 == 0) {
+				System.out.println("Os números dividido por 3 são: " + i);
+			}
+			i++;
+		}
+	}
+}
